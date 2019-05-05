0e98713a41bffb73be4ab44bdc928dc1d3067dd8
David Warren <ravy101@gmail.com>
2019-05-05 15:20:45 +1000

    no message

--- botris.py
+++ botris.py
@@ -1,32 +1,40 @@
 import pygame
+import numpy as np
 import gamesettings
 
-class BotrisGame(self):
+class BotrisGame(object):
         
     def __init__(self):
         pygame.init()
         self.clock = pygame.time.Clock()
+        self.human_player = False
         pygame.display.set_caption("Botris")
+        self.grid = np.zeros(gamesettings.GRID_DIM, dtype = int)
+        print(self.grid)
 
     def create_players(self, genomes = None, config = None):
         if genomes is not None and config is not None:
-                for g in genomes:
-                    pass   
-            else:      
-                pass
+            for g in genomes:
+                pass   
+        else:      
+            pass
+    
 
     def play(self):
         running = True
-        scroll_speed_boost = 0
         # ************************* MAIN LOOP *******************************
         while running:
+            self.clock.tick(gamesettings.FPS)
+            print(self.grid)
             pass
         # *******************************************************************
         
+    def draw_grid(self, screen, grid):
+        pass
 
         
 if __name__ == "__main__":
-    game = HopperGame()
+    game = BotrisGame()
     game.human_player = True
     game.create_players()
     game.play()   
